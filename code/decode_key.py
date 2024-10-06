e2dd7410c38655649da2411e86d3b18a2264e161
Chalermdej-l <chalermdej_l@hotmail.com>
2024-10-06 12:20:42 +0700

    feat: Add os to create folder

--- code/decode_key.py
+++ code/decode_key.py
@@ -1,9 +1,11 @@
 import base64
 import json
+import os
 import argparse
 def main(encode_key):
     base64_decode = base64.b64decode(encode_key)
     json_decode = json.loads(base64_decode.decode())
+    os.makedirs('./key',exist_ok=True)
     with open('./key/service_account.json','w+')as f:
         json.dump(json_decode,f,indent=4)
 
